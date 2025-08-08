9d231b4a21b88327df40d8d481a4aa0eb96e9b8b
MavisPhaphol <114840330+MavisPhaphol@users.noreply.github.com>
2025-08-08 00:36:16 -0400

    Update pingu.py
    Added lizard keyword 
    Updated ht and choose command

--- pingu.py
+++ pingu.py
@@ -6,14 +6,32 @@ intents.message_content = True  # Required for reading message content
 
 bot = commands.Bot(command_prefix="$", intents=intents)
 
-@bot.command()
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if "lizard" in message.content.lower(): #lizard without the syntax
+        lizardMeme="https://tenor.com/view/lizard-gif-4323854089874490655"
+        await message.channel.send(lizardMeme)
+    ##SEPARATE FROM IF
+    await bot.process_commands(message)
+
+@bot.command() #test command to ensure functionality
 async def test(ctx):
     await ctx.send("Test Successful")
 
-@bot.command()
+@bot.command() #heads or tails command
 async def ht(ctx,choice:str):
-    choice=choice.lower()
-    flip=random.choices(["heads","tails"])
+    userChoice=choice.lower()
+    flip=random.choice(["heads","tails"])
+    await ctx.send("Your Choice: "+userChoice+"\nResults: "+flip)
+
+@bot.command() #Random Selection command
+async def choose(ctx,choices:str):
+    options=choices.split()
+    randomSelection=random.choice(options)
+    await ctx.send("**PINGU SELECTS**: "+randomSelection)
 
 
 bot.run("*BOT TOKEN*")
+
