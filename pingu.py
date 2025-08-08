359a43642739ae3a852a66581f30b4a69daf1fc1
MavisPhaphol <MAVISPHAPHOL@GMAIL.COM>
2025-08-07 23:51:38 -0400

    Added Lizard Command Fixed ht Command Fixed choose Command

--- pingu.py
+++ pingu.py
@@ -1,3 +1,5 @@
+import random
+
 import discord
 from discord.ext import commands
 
@@ -6,14 +8,30 @@ intents.message_content = True  # Required for reading message content
 
 bot = commands.Bot(command_prefix="$", intents=intents)
 
-@bot.command()
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if "lizard" in message.content.lower(): #lizard without the syntax
+        lizardMeme="https://tenor.com/view/lizard-gif-4323854089874490655"
+        await message.channel.send(lizardMeme)
+    ##SEPARATE FROM IF
+    await bot.process_commands(message)
+
+@bot.command() #test command to ensure functionality
 async def test(ctx):
     await ctx.send("Test Successful")
 
 @bot.command()
-async def ht(ctx,choice:str):
-    choice=choice.lower()
-    flip=random.choices(["heads","tails"])
+async def ht(ctx,choice:str): #heads or tails command
+    userChoice=choice.lower()
+    flip=random.choice(["heads","tails"])
+    await ctx.send("Your Choice: "+userChoice+"\nResults: "+flip)
 
+@bot.command() #Random Selection command
+async def choose(ctx,choices:str):
+    options=choices.split()
+    randomSelection=random.choice(options)
+    await ctx.send("**PINGU SELECTS**: "+randomSelection)
 
-bot.run("*BOT TOKEN*")
+bot.run("**BOT TOKEN**") #bot goes online (will terminate when the file stops running)
